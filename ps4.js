8acb342fe9d30546073d07eaccb1366e6a83b8db
AlanXPG <80229568+AlanXPG@users.noreply.github.com>
2021-03-13 15:48:37 -0300

    Update ps4.js

--- ps4.js
+++ ps4.js
@@ -178,7 +178,7 @@ function setupRW() {
 }
 
 function toggle_payload(pld){
-	if(pld=="hen" || pld=="binloader"){if(confirm("O HEN (ou) BINLOADER não pode ser carregado com nenhuma outra carga útil  Você ainda deseja carregar esta carga sozinha?")){document.getElementById("pldooe").value = pld; load_payload();}}
+	if(pld=="hen" || pld=="binloader"){if(confirm("O Hen (ou) Binloader não pode ser carregado com nenhuma outra carga útil, você ainda deseja carregar esta carga sozinha?")){document.getElementById("pldooe").value = pld; load_payload();}}
 	else{
 		var pldooe = document.getElementById("pldooe").value;
 		var pldooe_full = document.getElementById("pldooe_full").value;
